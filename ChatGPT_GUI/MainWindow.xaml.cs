e81cd7fc3988801893d3ae901c4dc1f251646192
一个渣渣 <65759143+BlameTwo@users.noreply.github.com>
2023-04-01 20:46:00 +0800

    ok

--- ChatGPT_GUI/MainWindow.xaml.cs
+++ ChatGPT_GUI/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using ChatGPT_GUI.ViewModels;
 using SimpleUI.Controls;
 using System;
+using System.Windows.Automation.Peers;
+using System.Windows.Interop;
 
 namespace ChatGPT_GUI {
     /// <summary>
@@ -11,15 +13,22 @@ namespace ChatGPT_GUI {
             InitializeComponent();
             this.Loaded += MainWindow_Loaded;
             this.DataContext = vm;
-            this.WindowBackdrop = new SimpleUI.Utils.Backdrop.AcrylicBackdrop();
         }
+        private const int GWL_STYLE = -16;
+        private const int WS_SYSMENU = 0x80000;
 
         private void MainWindow_Loaded(object sender, System.Windows.RoutedEventArgs e) {
-            if(this.DataContext is MainViewModel vm) {
+
+            IntPtr hwnd = new WindowInteropHelper(this).Handle;
+            //SimpleUI.Win32.Win32.SetWindowLong(hwnd, (-20), 0x80);
+            //SimpleUI.Win32.Win32.SetWindowLong(hwnd, GWL_STYLE, SimpleUI.Win32.Win32.GetWindowLong(hwnd, GWL_STYLE) & ~WS_SYSMENU);
+            if (this.DataContext is MainViewModel vm) {
                 vm.ToastLitterMessage.ShowOwner = this.grid;
                 vm.ToastLitterMessage.ShowTime = TimeSpan.FromSeconds(5);
                 vm.AppNavigationViewService.Init(this.Framebase, true);
             }
         }
+
+        
     }
 }
