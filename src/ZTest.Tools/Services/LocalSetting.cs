176bc7167a38d1738d117d4660e8b7a8c3631b04
一个渣渣 <65759143+BlameTwo@users.noreply.github.com>
2023-05-05 17:07:49 +0800

    增加了Vmess解析和自定义配置

--- src/ZTest.Tools/Services/LocalSetting.cs
+++ src/ZTest.Tools/Services/LocalSetting.cs
@@ -1,113 +1,112 @@
 ﻿using System.Text.Json;
 using ZTest.Tools.Interfaces;
 
-namespace ZTest.Tools.Services {
-    public class LocalSetting : ILocalSetting {
-        public string LocalSettingFileName => "Setting.json";
+namespace ZTest.Tools.Services; 
+public class LocalSetting : ILocalSetting {
+    public string LocalSettingFileName => "Setting.json";
 
-        string filedir = Environment.GetFolderPath( Environment.SpecialFolder.MyDocuments);
+    string filedir = System.AppDomain.CurrentDomain.BaseDirectory;
 
-        string file {
-            get {
-                return Path.Combine(filedir, LocalSettingFileName);
-            }
+    string file {
+        get {
+            return Path.Combine(filedir, LocalSettingFileName);
         }
+    }
 
-        public Dictionary<string, object> Config { get; set; }
+    public Dictionary<string, object> Config { get; set; }
 
-        public async Task<bool> InitSetting() {
-            if (!File.Exists(Path.Combine(filedir, LocalSettingFileName))) {
-                await initWrite();
-                return true;
-            }
-            string filestr = File.ReadAllText(file);
-            if (!string.IsNullOrWhiteSpace(filestr)) {
-                await refresh();
-            }
+    public async Task<bool> InitSetting() {
+        if (!File.Exists(Path.Combine(filedir, LocalSettingFileName))) {
+            await initWrite();
             return true;
         }
-
-        private async Task initWrite() {
-            await Task.Run(() =>
-            {
-                File.WriteAllText(Path.Combine(filedir, LocalSettingFileName), "");
-            });
-        }
-
-        async Task refresh() {
-            await Task.Run(async () =>
-            {
-                StreamReader reader
-                 = new(file);
-                var str = reader.ReadToEnd();
-                reader.Close();
-                reader.Dispose();
-                if (!string.IsNullOrWhiteSpace(str))
-                {
-                    Config = JsonSerializer.Deserialize<Dictionary<string, object>>(str)!;
-                }
-                else
-                    Config = new();
-            });
-        }
-
-        public async Task<Object> ReadConfig(string key) {
+        string filestr = File.ReadAllText(file);
+        if (!string.IsNullOrWhiteSpace(filestr)) {
             await refresh();
-            if (Config.ContainsKey(key)) {
-                object ob = null;
-                Config.TryGetValue(key, out ob);
-                if (ob is JsonElement json)
-                    return Helper.JsonObjectHelper.ConvertObject(json);
-                else
-                    return null;
-            }
-            else {
-                return null;
-            }
         }
-
-
-        public async Task<T> ReadObjectConfig<T>(string key) {
-            await refresh();
-            if (Config.ContainsKey(key)) {
-                object ob = null;
-                Config.TryGetValue(key, out ob);
-                if (ob is JsonElement json && json.ValueKind == JsonValueKind.Object) {
-                    return json.Deserialize<T>();
-                }
-                else {
-                    throw new Exception("值类型请使用ReadConfig方法");
-                }
-            }
-            else {
-                return default(T);
-            }
-        }
-
-
-        public async Task SaveConfig<T>(string key, T value) {
-            await refresh();
-            if (Config.ContainsKey(key)) {
-                Config[key] = value;
-            }
-            else {
-                Config.Add(key, value);
-            }
-            save();
-        }
-
-        public void save() {
-            File.WriteAllText(Path.Combine(filedir, LocalSettingFileName), JsonSerializer.Serialize(Config));
-        }
-
-        public async Task<bool> DelectConfig(string key) {
-            if (Config.ContainsKey(key)) {
-                Config.Remove(key);
-                save();
-                return true;
-            }
-            return false;
-        }
-
+        return true;
     }
+
+    private async Task initWrite() {
+        await Task.Run(() =>
+        {
+            File.WriteAllText(Path.Combine(filedir, LocalSettingFileName), "");
+        });
+    }
+
+    async Task refresh() {
+        await Task.Run(async () =>
+        {
+            StreamReader reader
+             = new(file);
+            var str = reader.ReadToEnd();
+            reader.Close();
+            reader.Dispose();
+            if (!string.IsNullOrWhiteSpace(str))
+            {
+                Config = JsonSerializer.Deserialize<Dictionary<string, object>>(str)!;
+            }
+            else
+                Config = new();
+        });
+    }
+
+    public async Task<Object> ReadConfig(string key) {
+        await refresh();
+        if (Config.ContainsKey(key)) {
+            object ob = null;
+            Config.TryGetValue(key, out ob);
+            if (ob is JsonElement json)
+                return Helper.JsonObjectHelper.ConvertObject(json);
+            else
+                return null;
+        }
+        else {
+            return null;
+        }
+    }
+
+
+    public async Task<T> ReadObjectConfig<T>(string key) {
+        await refresh();
+        if (Config.ContainsKey(key)) {
+            object ob = null;
+            Config.TryGetValue(key, out ob);
+            if (ob is JsonElement json && json.ValueKind == JsonValueKind.Object) {
+                return json.Deserialize<T>();
+            }
+            else {
+                throw new Exception("值类型请使用ReadConfig方法");
+            }
+        }
+        else {
+            return default(T);
+        }
+    }
+
+
+    public async Task SaveConfig<T>(string key, T value) {
+        await refresh();
+        if (Config.ContainsKey(key)) {
+            Config[key] = value;
+        }
+        else {
+            Config.Add(key, value);
+        }
+        save();
+    }
+
+    public void save() {
+        File.WriteAllText(Path.Combine(filedir, LocalSettingFileName), JsonSerializer.Serialize(Config));
+    }
+
+    public async Task<bool> DelectConfig(string key) {
+        if (Config.ContainsKey(key)) {
+            Config.Remove(key);
+            save();
+            return true;
+        }
+        return false;
+    }
+
 }
