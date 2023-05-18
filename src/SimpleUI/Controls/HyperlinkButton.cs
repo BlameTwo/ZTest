2439295da07d2a8158aef7718625ce0479ec35c6
一个渣渣 <65759143+BlameTwo@users.noreply.github.com>
2023-05-18 11:41:24 +0800

    更新HyperlinkButton

---
+++ src/SimpleUI/Controls/HyperlinkButton.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xaml.Behaviors.Core;
+using SimpleUI.Base;
+using System.Diagnostics;
+using System.Windows;
+using static SimpleUI.Utils.Controls.IconResources;
+
+namespace SimpleUI.Controls;
+
+public class HyperlinkButton:ButtonBase
+{
+    protected override void OnClick()
+    {       
+        base.OnClick();
+        Process.Start("explorer.exe", Uri);
+    }
+
+
+    public string Uri
+    {
+        get { return (string)GetValue(UriProperty); }
+        set { SetValue(UriProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Uri.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty UriProperty =
+        DependencyProperty.Register("Uri", typeof(string), typeof(HyperlinkButton), new PropertyMetadata(null));
+
+
+    public FontIconEnum Icon
+    {
+        get { return (FontIconEnum)GetValue(IconProperty); }
+        set { SetValue(IconProperty, value); }
+    }
+
+    // Using a DependencyProperty as the backing store for Icon.  This enables animation, styling, binding, etc...
+    public static readonly DependencyProperty IconProperty =
+        DependencyProperty.Register("Icon", typeof(FontIconEnum), typeof(HyperlinkButton), new PropertyMetadata(null));
+
+}
